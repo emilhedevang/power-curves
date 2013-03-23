3e99945431a8e6ef1f220ad577571d8ea8ece23c
Andy Clifton <andy_clifton@yahoo.com>
2013-03-23 15:42:14 -0600

    Checking for necessary observation data
    Checking to see if the correct variables have been supplied to the
    program. The program will report an error to screen (but not stop) if a
    variable is not present. This check function is required if the code is
    to be distributed.

--- analysis/scripts/LoadObservations.R
+++ analysis/scripts/LoadObservations.R
@@ -1,3 +1,33 @@
 # read the observation data
 Obs <- read.csv(file=file.path(mainDir,TurbineName,ObservationsFile),header=TRUE,sep=",")
 Obs$SiteName <- SiteDir
+
+# now check for the presence of the variables we need
+VarList = c("WS_HH",
+            "Ti_HH",
+            "Shear",
+            "WS_Eq",
+            "RSS",
+            "Power_mean",
+            "Power_std")
+VarLongName = c("hub-height wind speed",
+                "hub-height turbulence intensity",
+                "shear",
+                "rotor-equivalent wind speed",
+                "accuracy of power-law fit",
+                "mean power",
+                "standard deviation of power")
+
+# and print results to screen
+cat(paste("Checking data in ", file.path(TurbineName,ObservationsFile)," \n",sep=""))
+for(Vari in seq(1,NROW(VarList),by=1))
+{
+  if(VarList[Vari] %in% colnames(Obs))
+  {
+    cat(paste("...Found ", VarLongName[Vari], " (", VarList[Vari], ")\n",sep=""));
+  }
+  else
+  {
+    cat(paste("...ERORR: did not find column ", VarLongName[Vari], ", (", VarList[Vari], ")\n",sep=""));
+  }
+}
